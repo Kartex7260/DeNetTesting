3a404484bfd26e589737c39c4bb2828c6aaedf31
kirillzhitko <kirillzhitko@gmail.com>
2024-04-18 18:54:57 +0300

    FIX: FAB не перекрывает нижний элемент

--- feat/node/src/main/java/kanti/denet/feat/node/ui/main/MainScreen.kt
+++ feat/node/src/main/java/kanti/denet/feat/node/ui/main/MainScreen.kt
@@ -4,6 +4,8 @@ import androidx.activity.compose.BackHandler
 import androidx.compose.animation.AnimatedVisibility
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
@@ -120,6 +122,10 @@ fun MainScreen(
                         }
                     }
                 )
+            }
+
+            item {
+                Spacer(modifier = Modifier.height(60.dp))
             }
         }
     }
