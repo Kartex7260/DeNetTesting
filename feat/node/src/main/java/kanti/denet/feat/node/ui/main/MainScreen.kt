f241338e52dff22a6102a89e5bf8f0f29cb01169
kirillzhitko <kirillzhitko@gmail.com>
2024-04-18 21:07:36 +0300

    FIX: отображение title в TopBar

--- feat/node/src/main/java/kanti/denet/feat/node/ui/main/MainScreen.kt
+++ feat/node/src/main/java/kanti/denet/feat/node/ui/main/MainScreen.kt
@@ -30,6 +30,7 @@ import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.hilt.navigation.compose.hiltViewModel
@@ -64,7 +65,11 @@ private fun TopBar(
     CenterAlignedTopAppBar(
         title = {
             val title = node?.hash?.asEthereumAddress ?: stringResource(id = R.string.root)
-            Text(text = title)
+            Text(
+                text = title,
+                maxLines = 1,
+                overflow = TextOverflow.Ellipsis
+            )
         },
         navigationIcon = {
             AnimatedVisibility(
